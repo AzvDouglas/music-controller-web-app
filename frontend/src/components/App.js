c02a116e28cb8666f4451f119367f940eb1f0458
AzvDouglas <douglasbep2014@gmail.com>
2021-12-13 23:59:29 -0300

    React Integration Using Webpack & Babel

---
+++ frontend/src/components/App.js
@@ -0,0 +1,14 @@
+import react, { Component, component} from "react";
+import { render } from "react-dom";
+
+export default class App extends Component {
+	constructor(props) {
+		super(pros);
+	}
+
+	render() {
+		return <h1>Testing React Code</h1>;
+	}
+}
+
+Const appDiv = document.getElementById("app")
